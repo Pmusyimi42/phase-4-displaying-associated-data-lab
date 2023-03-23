af29a1b20e69f55015a40d4c0833ae47e4cbd0bc
patrick kisyula <pmusyimi42@gmail.com>
2023-03-23 15:05:10 +0300

    Assignment Done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,6 @@
 class ItemsController < ApplicationController
+    def index
+        items = Item.all
+        render json: items, include: {user:{only: [:id, :username, :city]}}, except: [:created_at, :updated_at, :user_id]
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, include:{items:{only: [:id, :name, :description, :price]}}, except: [:created_at, :updated_at]
+        else
+            render json: {error:"User not found"}, status: :not_found
+        end
+    end
 end
